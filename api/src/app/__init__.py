878a2cc39077fc6457a9808cc24c4247300c6985
cc <cc@192.168.1.15>
2017-12-31 20:05:05 +0800

    Skip for video conversion which file already exist

--- api/src/app/__init__.py
+++ api/src/app/__init__.py
@@ -1,3 +1,4 @@
+import os.path
 import youtube_dl
 import yaml
 from flask import Flask, request, jsonify, send_from_directory
@@ -16,13 +17,19 @@ with open('./app/config.yml', 'r') as ymlfile:
 
 @app.route('/download', methods=['post'])
 def download():
+
     data = request.get_json(silent=True)
+    print('Request for download videoId: {}'.format(data['url']))
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([data['url']])
-        info_dict = ydl.extract_info(data['url'], download=False)
 
+        info_dict = ydl.extract_info(data['url'], download=False)
         filename = '{}.mp3'.format(info_dict.get('title', None))
+
+        if os.path.isfile('./storage/{}'.format(filename)):
+            print('The same file exist, skip convertion.')
+        else:
+            ydl.download([data['url']])
 
     return jsonify({
         'status': 'completed',
